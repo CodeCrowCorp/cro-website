ce69eb027065572ab08f49d47a2123000787080f
Gagan Suie <gagan.suie@codecrow.io>
2024-01-15 22:21:03 -0600

    Fix: discord url

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -21,9 +21,8 @@
 		<p
 			class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">
 			We push the boundaries of innovation through bleeding-edge technology, open-source
-			development, and the integration of AI and Web3.
+			development, and leveraging AI.
 		</p>
-
 		<div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
 			<a
 				href="https://github.com/CodeCrowCorp"
